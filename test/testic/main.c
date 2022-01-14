ccaa25dc890cad63e58d548751a4b8dd11692c2b
Anja Aćić <82903118+AnjaAcic@users.noreply.github.com>
2022-01-14 18:31:11 +0100

    Add files via upload

---
+++ test/testic/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "prijava_na_sistem.h"
+
+int main()
+{
+    //Testira se pravilna prijava za sefa
+    //Testira se pravilna prijava za radnika
+    //Testira se prijava sa pogresnim korisnickim imenom
+    //Testira se prijava sa pogresnom lozinkom
+    //Testira se n-ta prijava
+    prijavaNaSistem();
+
+    return 0;
+}
